ef51064bead936ba1294df572966a7249ca9a231
go2Cshop <shell800808@gmail.com>
2023-06-28 15:24:40 +0800

    feat: add mongoose & restaurantSeeder

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const exphbs = require("express-handlebars")
-const restaurantsData = require("./restaurant.json").results
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const Restaurant = require("./models/Restaurant")
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("mongodb error!")
+})
+
+db.once("open", () => {
+  console.log("mongodb connected!")
+})
+
 
 const app = express()
 const port = 3000
@@ -8,36 +30,41 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 
-// routes setting
+// 瀏覽全部餐廳
 app.get('/', (req, res) => {
-  res.render("index", { restaurantsData })
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => res.render("index", { restaurantsData }))
+    .catch(err => console.log(err))
 })
 
-app.get("/search", (req, res) => {
-  if (!req.query.keywords) {
-    return res.redirect("/")
-  }
+// app.get("/search", (req, res) => {
+//   if (!req.query.keywords) {
+//     return res.redirect("/")
+//   }
 
-  const keywords = req.query.keywords
-  const keyword = req.query.keywords.trim().toLowerCase()
+//   const keywords = req.query.keywords
+//   const keyword = req.query.keywords.trim().toLowerCase()
 
-  const filterRestaurantsData = restaurantsData.filter(
-    data =>
-      data.name.toLowerCase().includes(keyword) ||
-      data.category.includes(keyword)
-  )
-  res.render("index", { restaurantsData: filterRestaurantsData, keywords })
-})
+//   const filterRestaurantsData = restaurantsData.filter(
+//     data =>
+//       data.name.toLowerCase().includes(keyword) ||
+//       data.category.includes(keyword)
+//   )
+//   res.render("index", { restaurantsData: filterRestaurantsData, keywords })
+// })
 
-app.get("/restaurants/:restaurantId", (req, res) => {
-  const { restaurantId } = req.params
-  const restaurantData = restaurantsData.find(
-    data => data.id === Number(restaurantId)
-  )
-  res.render("show", { restaurantData })
-})
+// app.get("/restaurants/:restaurantId", (req, res) => {
+//   const { restaurantId } = req.params
+//   const restaurantData = restaurantsData.find(
+//     data => data.id === Number(restaurantId)
+//   )
+//   res.render("show", { restaurantData })
+// })
 
 // start and listen on the Express server
 app.listen(port, () => {
